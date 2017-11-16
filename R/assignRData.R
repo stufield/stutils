4e7feab5b90a21e094d11ed60a470114669a783e
Stu Field <wiki.kimi@gmail.com>
2017-11-16 14:36:09 -0700

    add assignRData(); reconfigure *.rda files to use a single R script and *.csv file; remove unnecessary *.rda files

---
+++ R/assignRData.R
@@ -0,0 +1,31 @@
+ 
+#' Assign RData Contents to a Variable
+#' 
+#' Assigns the contents of a binary RData or rda file to a variable
+#' rather than loading it directly into the global environment. Useful
+#' for avoiding unexpected collisions with variables in the current
+#' global environment.
+#' 
+#' @param file The character path to an \code{*.RData} or \code{*.rda} file.
+#' @return If the binary RData file only contains a single object then
+#' this object is returned. If the file contains more than one object,
+#' a named list is returned.
+#' @author Mike Mehan
+#' @seealso \code{\link{load}}.
+#' @examples
+#' 
+#' \dontrun{
+#' assignRData("path/to/myfile.rda")
+#' }
+#' 
+#' @export assignRData
+assignRData <- function(file) {
+   e <- new.env()
+   load(file, envir=e)
+   out <- as.list(e)
+   if ( length(out) == 1 ) {
+      out <- out[[1]]
+   }
+   return(out)
+}
+
