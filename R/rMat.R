7fe56b060cfcad7eb5c59d202ada37c36ac8b2b2
Stu Field <sfield@somalogic.com>
2018-04-07 14:51:14 -0600

    add .travis.yml to .Rbuildignore and fix documentation to rMat; try to get travis tracking up and running

--- R/rMat.R
+++ R/rMat.R
@@ -3,30 +3,26 @@
 #' 
 #' Generate a matrix with random numbers in its entries.
 #' 
-#' @param m Numeric. Row dimension of the matrix to be produced
-#' @param n Numeric. Column dimension of the matrix to be produced
-#' @param min Numeric. Minimum random number in range
-#' @param max Numeric. Maximum random number in range
+#' @param m Numeric. Row dimension of the matrix to be produced.
+#' @param n Numeric. Column dimension of the matrix to be produced.
+#' @param min Numeric. Minimum random number in range.
+#' @param max Numeric. Maximum random number in range.
 #' @param replace Logical. Should the sampled numbers be repeated?
-#' @param ... Additional arguments passed to \code{\link{matrix}}
+#' @param ... Additional arguments passed to \code{\link{matrix}}.
 #' @return A matrix with m x n dimensions full of random numbers
-#' on the interval [min, max]
+#' on the interval \verb{[min, max]}.
 #' @note Never know when you need a matrix of random numbers!
 #' @author Stu Field
 #' @seealso \code{\link[base]{sample}}
 #' @examples
 #' 
 #' rMat(5, 6, 35, 75)
-#' rMat(5, 6, 35, 75, replace=TRUE)
+#' rMat(5, 6, 35, 75, replace = TRUE)
 #' 
 #' @export rMat
-rMat <- function(m, n, min, max, replace=FALSE, ...) {
-
-   s <- sample(min:max, size=m*n, replace=replace)
-
-   if ( m*n!=length(s) )
-      stop("Incorrect dimensions or min/max length", call.=FALSE)
-
-   matrix(s, nrow=m, ncol=n, ...)
-
+rMat <- function(m, n, min, max, replace = FALSE, ...) {
+  s <- sample(min:max, size = m * n, replace = replace)
+  if ( m * n != length(s) )
+    stop("Incorrect dimensions or min/max length", call. = FALSE)
+  matrix(s, nrow = m, ncol = n, ...)
 }
