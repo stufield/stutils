e2a48d2fe9c9503d07fbcf3ef5f3f51d876339a5
Stu Field <stu.g.field@gmail.com>
2020-12-31 11:23:31 -0700

    Fix and clean up package data
    - reword documentation
    - proprely export data
    - minor re-work of objects themselves
    - move `popdata` out -> to new package
    - Fixes #3

--- R/data.R
+++ R/data.R
@@ -1,127 +1,90 @@
 #' Package Objects
 #'
-#' @description Below is a listing of all the objects contained in the
-#' \pkg{stutils} package. There is a short description of
-#' each object, and perhaps a description of its elements, how they were
-#' derived, and their sources.
+#' The objects contained in the \pkg{stutils} package.
 #' If known, the authorship of the object is attributed.
+#'
 #' @name objects
 NULL
 
+#' @describeIn objects
+#' A sample `tibble` data frame for running examples and checking
+#' functions that act on data frames. See `?tibble`.
+#' @format **test-data**: An object of class `tbl_df`
+#' (inherits from `tbl`, `data.frame`) with 40 rows and 12 columns and
+#' defined as follows:
+#' \tabular{ll}{
+#'   Variable    \tab Definition \cr
+#'   pid         \tab `sprintf("%02i", 1:40)`  \cr
+#'   Pop         \tab `rep(head(LETTERS, 10), 4)` \cr
+#'   Sample      \tab `factor(sample(c("small", "medium", "large"), 20))` \cr
+#'   TimePoint   \tab `factor(rep(c("baseline", "6 mths", "12 mths", "24 mths"), each = 10))` \cr
+#'   a           \tab `round(rnorm(40, mean = 25, sd = 3.5), 0)` \cr
+#'   b           \tab `sample(0:6, 40, replace = TRUE)` \cr
+#'   seq.1234.56 \tab `rnorm(40, mean = 25, sd = 3.5)` \cr
+#'   seq.6969.42 \tab `rnorm(40, mean = 25, sd = 1.5)` \cr
+#'   x           \tab `rnorm(40, mean = 25, sd = 3)` \cr
+#'   y           \tab `round(runif(40, 10, 20), 0)` \cr
+#'   z           \tab `round(rnrom(40, mean = 1, sd = 0.5), 3)` \cr
+#'   Response    \tab `factor(sample(c("control", "disease"), 40, replace = TRUE)))` \cr
+#' }
+#' @source **test_data**: Stu Field
+#' @examples
+#' test_data
+#' @usage test_data
+"test_data"
 
 #' @describeIn objects
 #' Data set containing tree characteristic metric data,
 #' that has been blinded and anonymized from its original source.
 #' Used heavily in the `R` tutorial and for general data
-#' frame usage (e.g. package testing). The format is "tibble" data
-#" frame containing 20 cases and numerous random variables
-#' @references `TreeData`: originally from Sala et al., but heavily modified to be
-#' unrecognizable and used as the main example data set for the EEID R tutorial.
-#' @source TreeData: Stu Field
+#' frame usage (e.g. package testing). The format is `tibble` data
+#" frame containing 20 cases and numerous random variables.
+#' @references `tree_data`: originally from Sala et al., but heavily modified
+#' to obscure its original source data values.
+#' @format **tree_data**: An object of class `tbl_df` (inherits from `tbl`,
+#' `data.frame`) with 20 rows and 10 columns.
+#' @source **tree_data**: Sala et. al. \url{http}
 #' @examples
-#' head(TreeData)
-#' summary(TreeData)
-#' suppressWarnings(sapply(TreeData, mean))
+#' summary(tree_data)
+#' purrr::map_if(tree_data, is.numeric, mean)
 #'
-#' @usage TreeData
-TreeData <- function() NULL
-
-
-#' @describeIn objects
-#' A data set it the data produced by the
-#' projection for the 6 class example used in the EEID R tutorial.
-#' The format is a matrix of the 6-class population projection with classes
-#' as columns and the map/step solutions as the rows
-#' @references `popdata`: Stu Field (The EEID R Tutorial)
-#' @source popdata: Stu Field
-#' @examples
-#' head(popdata, 10)
-#'
-#' @usage popdata
-popdata <- function() NULL
-
+#' @usage tree_data
+"tree_data"
 
 #' @describeIn objects
 #' Coefficient Interclass Correlation Data Example
 #' from Sokal & Rohlf (Biometry; 3rd ed.), pages 210-214.
-#' The format is a data frame of data with treatments as
-#' columns and rows as  cases.
-#' @references `Ri_data`: Sokal & Rohlf (Biometry; 3rd ed.), p. 210-214.
+#' @format **Ri_data**: An object of class data.frame with 13 rows and 4 columns.
+#' @source **Ri_data**: Sokal & Rohlf (Biometry; 3rd ed.), p. 210-214.
 #' @examples
 #' Ri_data
 #'
 #' @usage Ri_data
-Ri_data <- function() NULL
-
-
-#' @describeIn objects
-#' A vector containing 1000 Monte-Carlo simulation
-#' estimates of the integral estimate (area under curve) for a
-#' given function. The simulation is time greedy so these
-#' data are saved as an object.
-#' The format is a numeric vector of length = 1000.
-#' @references `MC_sims`: Bill Black IV
-#' @examples
-#' head(MC_sims)
-#' tail(MC_sims)
-#' mean(MC_sims)
-#' hist(MC_sims)
-#'
-#' @usage MC_sims
-MC_sims <- function() NULL
-
+"Ri_data"
 
 #' @describeIn objects
 #' Infection data describing the infections
 #' count data of an infectious disease
-#' by geographic position. Includes the following headings: \cr
-#' \itemize{
-#'   \item CountyNo
-#'   \item CountyNo
-#'   \item CountyName
-#'   \item State
-#'   \item Fips
-#'   \item Site
-#'   \item Lat
-#'   \item Long
-#'   \item Population
-#'   \item Area
-#'   \item Density
-#'   \item Infected
-#' }
-#' A "tibble" data frame containing 3082 individual cases/records.
+#' by geographic position. Includes the following variables:
+#'   * CountyNo
+#'   * CountyNo
+#'   * CountyName
+#'   * State
+#'   * Fips
+#'   * Site
+#'   * Lat
+#'   * Long
+#'   * Population
+#'   * Area
+#'   * Density
+#'   * Infected
+#' @format **InfectionByCounty**: An object of class `tbl_df` (inherits
+#' from `tbl`, `data.frame`) with 3071 rows and 11 columns.
 #' @references `InfectionByCounty`: originally from Dylan George and
-#' used for an exercise data set in the EEID R tutorial (2010).
-#' @source InfectionByCounty: Dylan George
+#' used for an exercise data set in the 2010 EEID R tutorial.
+#' @source **InfectionByCounty**: Dylan George
 #' @examples
-#' head(InfectionByCounty)
+#' InfectionByCounty
 #'
 #' @usage InfectionByCounty
-InfectionByCounty <- function() NULL
-
-
-#' @describeIn objects
-#' A quick sample tibble data frame for
-#' running examples and checking data frame functionalities.
-#' See `?tibble`.
-#' \tabular{lr}{
-#'   Variable       \tab Definition \cr
-#'   pid            \tab `1041:1080`  \cr
-#'   Pop            \tab `rep(head(LETTERS, 10), 4)` \cr
-#'   Sample         \tab `factor(sample(c("small", "medium", "large"), 20))` \cr
-#'   TimePoint      \tab `factor(rep(c("baseline", "6 mths", "12 mths", "24 mths"), each = 10))` \cr
-#'   a              \tab `round(rnorm(40, mean = 25, sd = 3.5), 0)` \cr
-#'   b              \tab `sample(0:6, 40, replace = TRUE)` \cr
-#'   ABCD.1234.56.8 \tab `rnorm(40, mean = 25, sd = 3.5)` \cr
-#'   XYZZ.6969.4.7  \tab `rnorm(40, mean = 25, sd = 1.5)` \cr
-#'   x              \tab `rnorm(40, mean = 25, sd = 3)` \cr
-#'   y              \tab `round(runif(40, 10, 20), 0)` \cr
-#'   z              \tab `round(rnrom(40, mean = 1, sd = 0.5), 3)` \cr
-#'   Response       \tab `factor(sample(c("control", "disease"), 40, replace = TRUE)))` \cr
-#' }
-#' @source test_data: Stu Field
-#' @examples
-#' dim(test_data)
-#' head(test_data)
-#' @usage test_data
-test_data <- function() NULL
+"InfectionByCounty"
